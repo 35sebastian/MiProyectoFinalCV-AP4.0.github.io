e9e8964814abbb6aae468e3c281606bd62f889d3
Sebatián Ariel Vega <vegaa.sebastian@gmail.com>
2023-01-30 17:59:10 -0300

    Comprobación de upload 1

--- functions.js
+++ functions.js
@@ -2,6 +2,6 @@
 console.log ("functions.js")    // Lo uso para saber si el archivo html está conectado correctamente al .js
 
 document.getElementById('boton').onclick = function (){
-    console.log("Capturamos el evento Click")
+    console.log("Capturamos Click")
     document.getElementById("demo").innerHTML = "Probando botoncito"
 }
